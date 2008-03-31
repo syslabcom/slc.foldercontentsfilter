0faa48de375c260113a181f4a42e1e3470f616a2
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-03-31 13:38:22 +0000

    added depth limit

--- slc/foldercontentsfilter/browser/foldercontents.py
+++ slc/foldercontentsfilter/browser/foldercontents.py
@@ -1,16 +1,28 @@
-from plone.app.content.browser.foldercontents import FolderContentsView
+from plone.app.content.browser.foldercontents import FolderContentsView, FolderContentsTable
+from plone.app.content.browser.interfaces import IFolderContentsView
+from zope.interface import implements
 
 class FolderContentsFilterView(FolderContentsView):
     """ """
+    implements(IFolderContentsView)
+
     def contents_table(self):
-        import pdb;pdb.set_trace()
+        title = self.request.get('Title', '').strip()
+        if title != '':
+            title = title + '*'
+        path = self.request.get('path', '')
+        depth = self.request.get('depth',1)
+        if title:
+            contentFilter = { 'Title': title
+                            , 'path': { 'query': path
+                                      , 'depth': depth}
+                            }
+        else:
+            contentFilter = {}
+            
         table = FolderContentsTable(self.context, 
-                        self.request, 
-                        contentFilter = {  'Title': self.request.Title
-                                            , 'path': { 'query':self.request.path
-                                                      , 'depth': self.request.get('depth',1)}
-                              
-                                        }
-                                    )
+                                    self.request, 
+                                    contentFilter = contentFilter
+                                   )
         return table.render()
     
