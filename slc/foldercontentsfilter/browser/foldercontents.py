bc284ef0fe47f0b4e7dc01b82b7eb2016cbfbfcc
pilz <pilz@97d050b9-b799-4550-a901-4f04598d4601>
2008-04-02 18:13:00 +0000

    added table.pt and overwrite it to add sorting headers

--- slc/foldercontentsfilter/browser/foldercontents.py
+++ slc/foldercontentsfilter/browser/foldercontents.py
@@ -1,12 +1,19 @@
-from plone.app.content.browser.foldercontents import FolderContentsView, FolderContentsTable
-from plone.app.content.browser.interfaces import IFolderContentsView
+import Acquisition
 from zope.interface import implements
+#from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.app import pagetemplate 
+
+from plone.app.content.browser.foldercontents import FolderContentsView, FolderContentsTable
+from plone.app.content.browser.tableview import Table, TableKSSView
+from plone.app.content.browser.interfaces import IFolderContentsView
+
 
 class FolderContentsFilterView(FolderContentsView):
     """ """
     implements(IFolderContentsView)
 
     def contents_table(self):
+        """ allow to pass params to contentFilter """
         title = self.request.get('Title', '').strip()
         if title != '':
             title = title + '*'
@@ -20,9 +27,54 @@ class FolderContentsFilterView(FolderContentsView):
         else:
             contentFilter = {}
             
-        table = FolderContentsTable(self.context, 
+        sort_on = self.request.get('sort_on', 'sortable_title')
+        if sort_on!='':
+            contentFilter['sort_on'] = sort_on
+
+        sort_order = self.request.get('sort_order', '')
+        if sort_order!='':
+            contentFilter['sort_order'] = sort_order
+            
+        table = FCFFolderContentsTable(self.context, 
                                     self.request, 
                                     contentFilter = contentFilter
                                    )
         return table.render()
+
+
     
+class FCFFolderContentsTable(FolderContentsTable):
+    """   
+    The foldercontents table renders the table and its actions.
+    """                
+
+    def __init__(self, context, request, contentFilter={}):
+        self.context = context
+        self.request = request
+        self.contentFilter = contentFilter
+
+        url = self.context.absolute_url()
+        view_url = url + '/@@folder_contents'
+        self.table = FCFTable(request, url, view_url, self.items,
+                           show_sort_column=self.show_sort_column,
+                           buttons=self.buttons)   
+                           
+    def render(self):
+        render = self.table.render
+        
+        return self.table.render()
+
+
+class FCFFolderContentsKSSView(TableKSSView):
+    table = FCFFolderContentsTable
+                
+                
+
+class FCFTable(Table):
+    """   
+    The table renders a table with sortable columns etc.
+
+    It is meant to be subclassed to provide methods for getting specific table info.
+    """                
+
+    render = pagetemplate.ViewPageTemplateFile("templates/table.pt")
--- slc/foldercontentsfilter/browser/viewlets.py
+++ slc/foldercontentsfilter/browser/viewlets.py
@@ -1,5 +1,8 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import SearchBoxViewlet
 
+
+
 class FolderContentsSearchBoxViewlet(SearchBoxViewlet):
     render = ViewPageTemplateFile('templates/searchbox.pt')
+    
